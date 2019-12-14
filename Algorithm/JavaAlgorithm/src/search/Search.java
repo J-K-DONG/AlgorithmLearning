15949bc87bd4c0ed0bf6eb245f2bde2b86cb7fe8
J-K-DONG <dongjinkun99@gmail.com>
2019-12-15 00:37:45 +0800

    binary search

--- Algorithm/JavaAlgorithm/src/search/Search.java
+++ Algorithm/JavaAlgorithm/src/search/Search.java
@@ -1,13 +1,14 @@
-package com.search;
+package search;
 import java.util.Arrays;
 
 public class Search {
 
-    public static int Rank(int key, int[] a) {
+    public static int BinarySearch_1(int key, int[] a) {
         int lo = 0;
         int hi = a.length - 1;
-        while (true)
-        {
+        int count = 0;
+        while (true) {
+            count++;
             if (lo > hi) {
                 break;
             }
@@ -15,20 +16,46 @@ public class Search {
             if (key < a[mid]) {
                 hi = mid - 1;
             } else if (key > a[mid]) {
-                hi = mid + 1;
-            }
-            else {
+                lo = mid + 1;
+            } else {
+                System.out.println("循环了 " + count + " 次");
                 return mid;
             }
         }
         return -1;
     }
 
+    // 二分查找的递归方法
+    public static int BinarySearch_2(int key, int[] a) {
+        int index = BinarySearchRank(key, a, 0, a.length - 1);
+        return index;
+    }
+
+    public static int BinarySearchRank(int key, int[] a, int begin, int end) {
+        if (begin > end) {
+            return -1;
+        }
+        int mid = (begin + end) / 2;
+        int index = -1;
+        if (key < a[mid]) {
+            index = BinarySearchRank(key, a, begin, mid - 1);
+        }
+        else if (key > a[mid]) {
+            index = BinarySearchRank(key, a, mid + 1, end);
+        }
+        else {
+            index = mid;
+        }
+        return index;
+    }
+
+
+
     public static void main(String[] args) {
-        int[] whilelist = {3,8,6,1,1,8,6,3,8};
+        int[] whilelist = {2,5,8,13,16,17,21,34,66};
 
         Arrays.sort(whilelist);
-        int data = Rank(6, whilelist);
+        int data = BinarySearch_2(17, whilelist);
         System.out.println(data);
     }
 }
