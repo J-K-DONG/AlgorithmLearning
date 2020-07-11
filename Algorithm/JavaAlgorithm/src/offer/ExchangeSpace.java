329dff84373f3ba1c040f8452d0ef9f3120ecdd3
J-K-DONG <dongjinkun99@gmail.com>
2020-07-11 20:21:52 +0800

    Using StringBuilder

---
+++ Algorithm/JavaAlgorithm/src/offer/ExchangeSpace.java
@@ -0,0 +1,33 @@
+/**
+面试题05. 替换空格
+
+请实现一个函数，把字符串 s 中的每个空格替换成"%20"。
+
+示例 1：
+
+输入：s = "We are happy."
+输出："We%20are%20happy."
+**/
+
+
+package offer;
+
+public class ExchangeSpace{
+    public static String exchangeSpace(String str){
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) == ' ') {
+                sb.append("%20");
+            }else {
+                sb.append(str.charAt(i));
+            }
+        }
+        return sb.toString();
+    }
+
+    public static void main(String[] args) {
+        String str = "a b c";
+        String res = exchangeSpace(str);
+        System.out.println(res);
+    }
+}
